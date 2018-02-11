d29451c55556e695a48216f1b5ac31dbfcef6101
lunawolf43 <mwolf907@gmail.com>
2018-02-11 03:21:10 -0500

    Added Flora Class

--- src/bio/Flora.java
+++ src/bio/Flora.java
@@ -1,6 +1,10 @@
 package bio;
 
-public class Flora {
+import java.awt.Graphics;
+
+import gameEngine.Drawable;
+
+public class Flora implements Drawable{
 	
 	int DNA[];
 	int DNAstruct[];
@@ -10,6 +14,15 @@ public class Flora {
 	int mat_ore_consum;
 	
 	public Flora(int fatherDNA[], int motherDNA[]){
+		DNA = new int[fatherDNA.length * 2];
+		
+		
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
 		
 	}
 
