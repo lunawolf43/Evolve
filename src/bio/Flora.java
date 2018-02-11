4e022f78dd17fca7fc9325d3246c07ad74c16c12
lunawolf43 <mwolf907@gmail.com>
2018-02-11 02:47:04 -0500

    Flora class added

---
+++ src/bio/Flora.java
@@ -0,0 +1,16 @@
+package bio;
+
+public class Flora {
+	
+	int DNA[];
+	int DNAstruct[];
+	int weight;
+	int ej_consumption;
+	int ej_potential;
+	int mat_ore_consum;
+	
+	public Flora(int fatherDNA[], int motherDNA[]){
+		
+	}
+
+}
